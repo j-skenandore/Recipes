130a7255f635041158611a5812e7c94f765962eb
Jordan Skenandore <jordan.skenandore@gmail.com>
2020-03-13 20:18:35 -0600

    Initial storyboard implemented. All outlets are connected to the corresponding view controller files

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by J-Skenandore on 3/13/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func searchTextFieldEdited(_ sender: Any) {
+    }
+    
+}
